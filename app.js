418eaaf5b7ac003cd100db78bf3e8dd2b7549d07
Claudia_Alves <claudiascarvalves@gmail.com>
2023-11-09 15:44:30 +0000

    game logi added

---
+++ app.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition 
+let currentTime = 60
+let timerId = null
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove("mole")
+  })
+
+  let randomPosition = squares[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
+
+  hitPosition = randomPosition.id
+}
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+      result++
+      score.textContent = result
+      hitPosition = null
+    }
+  })
+})
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 600)
+}
+moveMole()
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId)
+    clearInterval(timerId)
+    alert('TIME IS UP! Your final score is ' + result)
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
+
+
+
